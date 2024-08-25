4215c9744e714534e24a82fd433f65a169ca3668
Ismah Hussain <ismahhussain@hotmail.co.uk>
2024-08-25 18:49:48 +0100

    login system and dashboard redirection

--- src/main/java/fish/payara/hello/jsf/LoginBean.java
+++ src/main/java/fish/payara/hello/jsf/LoginBean.java
@@ -1,23 +1,60 @@
 package fish.payara.hello.jsf;
-import jakarta.ejb.EJB;
+import fish.payara.hello.service.LoginService;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
-import java.util.List;
 
 @Named(value = "loginBean")
 @RequestScoped
 public class LoginBean {
 
-//    public String username = "";
-//
-//    public LoginBean() {
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
+    @Inject
+    private LoginService loginService;
+
+    private String username;
+    private String password;
+    private String message;
+
+    public LoginBean() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    /*
+     this works because of how jsf handles it,
+     don't need to specify in faces-config.xml
+     */
+
+    public String checkForAccount(){
+        if (loginService.checkForAccount(username, password)) {
+            try {
+                FacesContext.getCurrentInstance().getExternalContext().redirect("dashboard.xhtml");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        } else {
+            message = "Invalid username or password";
+            return "login";
+        }
+    }
 }
