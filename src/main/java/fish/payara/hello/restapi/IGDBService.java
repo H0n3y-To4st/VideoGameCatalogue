f6bf9d513ac19daaea8d640fcaaa442700fd61ad
Ismah Hussain <ismahhussain@hotmail.co.uk>
2025-01-07 21:57:26 +0000

    Created simple MP client.

--- src/main/java/fish/payara/hello/restapi/IGDBService.java
+++ src/main/java/fish/payara/hello/restapi/IGDBService.java
@@ -1,105 +1,41 @@
 package fish.payara.hello.restapi;
 
 import fish.payara.hello.entities.Games;
+import fish.payara.hello.restapi.client.IGDBClient;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.client.Client;
-import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.Entity;
-import jakarta.ws.rs.client.WebTarget;
-import jakarta.ws.rs.core.GenericType;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
-import java.util.Collections;
 import java.util.List;
 
 @ApplicationScoped
 public class IGDBService {
 
-    //TODO: set these as environment variables
-    private String CLIENT_ID = "13itn7518vwktd1u8ifuc3oaik3z1s";
-    private String ACCESS_TOKEN = "Bearer r4x03x3eqqnkprmxmiihpulz96z2gc";
-
-    //TODO: microprofile rest client
-//    @Inject
-//    @RestClient
-//    private IGDBClient igdbClient;
+    @Inject
+    @RestClient
+    private IGDBClient igdbClient;
 
     public List<Games> getTopGames() {
-        Client client = ClientBuilder.newClient();
-        WebTarget target = client.target("https://api.igdb.com/v4/games");
-
         String body = "fields name,genres.name,rating,category, cover.url;\n" +
                 "where rating >= 90 & category = 0 & themes != (42); sort rating_count desc;\n" +
                 "limit 24;";
-        Response response = target.request(MediaType.APPLICATION_JSON)
-                .header("Client-ID", CLIENT_ID)
-                .header("Authorization", ACCESS_TOKEN)
-                .post(Entity.json(body));
-
-        List<Games> games = Collections.emptyList();
-        if (response.getStatus() == 200) {
-            games = response.readEntity(new GenericType<List<Games>>() {});
-        } else {
-            System.out.println("Error: " + response.getStatus());
-        }
-        response.close();
-        client.close();
-
-        return games;
+        return igdbClient.getTopGames(body);
     }
 
     public List<Games> getGameByID(int gameId) {
-        Client client = ClientBuilder.newClient();
-        WebTarget target = client.target("https://api.igdb.com/v4/games");
-
         String body = "fields id, name, genres.name, rating, cover.url; where id = " + gameId + ";";
-        Response response = target.request(MediaType.APPLICATION_JSON)
-                .header("Client-ID", CLIENT_ID)
-                .header("Authorization", ACCESS_TOKEN)
-                .post(Entity.json(body));
-
-        List<Games> games = response.readEntity(new GenericType<List<Games>>() {});
-        response.close();
-        client.close();
-        return games;
+        return igdbClient.getGameByID(body);
     }
 
     public List<Games> searchGamesByName(String gameName, boolean advancedSearch) {
-        Client client = ClientBuilder.newClient();
-        WebTarget target = client.target("https://api.igdb.com/v4/games");
-
-        String body = "fields name,genres.name,rating, cover.url;\n"+
+        String body = "fields name,genres.name,rating, cover.url;\n" +
                 "where name ~ *\"" + gameName + "\"* & category = 0 & themes != (42); limit 3;";
-//
-//        if (!advancedSearch) {
-//            body += "limit 3;";
-//        }
-        Response response = target.request(MediaType.APPLICATION_JSON)
-                .header("Client-ID", CLIENT_ID)
-                .header("Authorization", ACCESS_TOKEN)
-                .post(Entity.json(body));
-
-        List<Games> games = response.readEntity(new GenericType<List<Games>>() {});
-        response.close();
-        client.close();
-        return games;
+        return igdbClient.searchGamesByName(body);
     }
 
     public Games getSelectedGameDetails(int gameId) {
-        Client client = ClientBuilder.newClient();
-        WebTarget target = client.target("https://api.igdb.com/v4/games");
-
-        String body = "fields name,genres.name,rating,summary,cover.url,screenshots.url;\n"
-                + "where id = " + gameId + ";";
-        Response response = target.request(MediaType.APPLICATION_JSON)
-                .header("Client-ID", CLIENT_ID)
-                .header("Authorization", ACCESS_TOKEN)
-                .post(Entity.json(body));
-
-        Games game = response.readEntity(new GenericType<Games>() {});
-        response.close();
-        client.close();
-        return game;
+        String body = "fields name,genres.name,rating,summary,cover.url,screenshots.url;\n" +
+                "where id = " + gameId + ";";
+        return igdbClient.getSelectedGameDetails(body);
     }
 }
--- src/main/java/fish/payara/hello/restapi/client/IGDBClient.java
+++ src/main/java/fish/payara/hello/restapi/client/IGDBClient.java
@@ -1,23 +1,37 @@
 package fish.payara.hello.restapi.client;
+
 import fish.payara.hello.entities.Games;
-
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import java.util.List;
 
-//@RegisterRestClient(baseUri = "https://api.igdb.com/v4")
+@RegisterRestClient(baseUri = "https://api.igdb.com/v4/games")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@ClientHeaderParam(name = "Client-ID", value = "{clientId}")
+@ClientHeaderParam(name = "Authorization", value = "{accessToken}")
 public interface IGDBClient {
 
-//    @POST
-//    @Path("/games")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    List<Games> getTopGames(@HeaderParam("Client-ID") String clientId,
-//                            @HeaderParam("Authorization") String accessToken);
+    default String clientId() {
+        return ConfigProvider.getConfig().getValue("CLIENT_ID", String.class);
+    }
+
+    default String accessToken() {
+        return ConfigProvider.getConfig().getValue("ACCESS_TOKEN", String.class);
+    }
+
+    @POST
+    List<Games> getTopGames(String body);
+
+    @POST
+    List<Games> getGameByID(String body);
+
+    @POST
+    List<Games> searchGamesByName(String body);
+
+    @POST
+    Games getSelectedGameDetails(String body);
 }
